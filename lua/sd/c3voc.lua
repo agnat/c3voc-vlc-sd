027cf932df8681e21e0980c97b2468bacc4643af
David Siegel <agnat@me.com>
2016-01-02 14:49:47 +0100

    cleaned up

--- lua/sd/c3voc.lua
+++ lua/sd/c3voc.lua
@@ -1,6 +1,3 @@
---[[
---]]
-
 local sidebar_title = "C3VOC Streaming"
 local base_url      = "http://localhost:1337" -- "https://streaming.events.ccc.de"
 local events_path   = "events/v1.json"
@@ -9,75 +6,85 @@ local streams_path  = "streams/v1.json"
 function events_url()
   return base_url .. "/" .. events_path
 end
+
 function streams_url(event_slug)
   return base_url .. "/" .. event_slug .. "/" .. streams_path
 end
 
+-- See vlc/share/lua/sd/README.txt
 local lazily_loaded = false
 
 local dkjson = nil
-local json = {}
-json["parse_url"] = function(url)
-  vlc.msg.info("Fetching " .. url)
-  local stream = vlc.stream(url)
-  local string = ""
-  local line   = ""
+local common = nil
+local json =
+{ parse_url = function(url)
+    vlc.msg.info("Fetching " .. url)
+    local stream = vlc.stream(url)
+    local string = ""
+    local line   = ""
 
-  repeat
-    line = stream:readline()
-    string = string..line
-  until line ~= nil
+    repeat
+      line = stream:readline()
+      string = string .. line
+    until line ~= nil
 
-  return dkjson.decode(string)
-end
+    return dkjson.decode(string)
+  end
+}
 
 function lazy_load()
   if lazily_loaded then return nil end
   dkjson = require "dkjson"
+  common = require "common"
   lazily_loaded = true
-end
-
-function descriptor()
-  return { title = sidebar_title }
 end
 
 function dropnil(s)
   if s == nil then return "" else return s end
 end
 
-function add_group_members(parent, members, streams)
+function add_stream(parent, title, url)
+  parent:add_subitem({ title = title
+                     , path = url
+                     });
+end
+
+function add_translations(parent, room)
+  add_stream(parent, "Native", room["url"]);
+  add_stream(parent, "Live Translation", room["url"]);
+end
+
+function add_rooms(parent, members, streams)
   for _,member in ipairs(members) do
-  vlc.msg.info("member " .. member)    
     local room = streams["rooms"][member];
-    parent:add_subnode({title = room["name"]});
+    local room_name = room["name"];
+    if room["translation"] then
+      local room_node = parent:add_subnode({title = room_name});
+      add_translations(room_node, room)
+    else
+      add_stream(parent, room_name, room["url"]);
+    end
   end
 end
 
-function add_streams(parent, slug)
+function add_groups(parent, slug)
   local streams = json.parse_url(streams_url(slug))
-  for group,members in pairs(streams["groups"]) do
+  for group,members in common.pairs_sorted(streams["groups"]) do
     local group_node = parent:add_subnode({title = group});
-    add_group_members(group_node, members, streams)
+    add_rooms(group_node, members, streams)
   end
-end
-
-function add_stream(parent, stream)
-  parent:add_subitem(
-    { path = "https://"
-    , title = stream["name"]
-      --"url" = 
-      --"language" = 
-      --"nowplaying" = 
-      --"publisher" = 
-      --"encodedby" = 
-      --"arturl" = 
-      --"options" = 
-      --"meta" = 
-    })
 end
 
 function event_is_in_progress(event)
   return true
+end
+
+-- Service Discovery API Functions
+
+function descriptor()
+  return { title = sidebar_title
+         , capabilities = {}
+         }
 end
 
 function main()
@@ -88,35 +95,7 @@ function main()
   for _,event in ipairs(events) do
     local parent = vlc.sd.add_node({ title = event["name"] })
     if (event_is_in_progress(event)) then
-      add_streams(parent, event["slug"])
+      add_groups(parent, event["slug"])
     end
   end
-
---[[
-  local tree = simplexml.parse_url("http://dir.xiph.org/yp.xml")
-  for _, station in ipairs( tree.children ) do
-    simplexml.add_name_maps( station )
-    local station_name = station.children_map["server_name"][1].children[1]
-    if station_name == "Unspecified name" or station_name == "" or station_name == nil
-    then
-      station_name = station.children_map["listen_url"][1].children[1]
-      if string.find( station_name, "radionomy.com" )
-      then
-        station_name = string.match( station_name, "([^/]+)$")
-        station_name = string.gsub( station_name, "-", " " )
-      end
-    end
-    vlc.sd.add_item( {path=station.children_map["listen_url"][1].children[1],
-                      title=station_name,
-                      genre=dropnil(station.children_map["genre"][1].children[1]),
-                      nowplaying=dropnil(station.children_map["current_song"][1].children[1]),
-                      uiddata=station.children_map["listen_url"][1].children[1]
-                              .. dropnil(station.children_map["server_name"][1].children[1]),
-                      meta={["Listing Source"]="dir.xiph.org",
-                            ["Listing Type"]="radio",
-                            ["Icecast Bitrate"]=dropnil(station.children_map["bitrate"][1].children[1]),
-                            ["Icecast Server Type"]=dropnil(station.children_map["server_type"][1].children[1])
-                          }} )
-  end
-  --]]
 end
--- server.js
+++ server.js
@@ -5,11 +5,13 @@ var server = http.createServer(function (request, response) {
   if (request.url == "/events/v1.json") {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end(JSON.stringify(
-    [ { "name": "32nd Chaos Communication Congress - Gated Communities"
+    [ { "name": "32nd Chaos Communication Congress – Gated Communities"
       , "slug": "32c3"
+      , "state": "running"
       }
-    , { "name": "33nd Chaos Communication Congress - [Ba]king [Br]ead"
+    , { "name": "33nd Chaos Communication Congress – [Ba]king [Br]ead"
       , "slug": "33c3"
+      , "state": "upcoming"
       }
     ]));
   } else if (request.url == "/32c3/streams/v1.json" || request.url == "/33c3/streams/v1.json") {
@@ -21,15 +23,51 @@ var server = http.createServer(function (request, response) {
       , "Live Podcasts": ["sendezentrum", "podcastertisch"]
       }
     , "rooms": 
-      { "hall1": {"name": "Hall 1"}
-      , "hall2": {"name": "Hall 2"}
-      , "hall6": {"name": "Hall 6"}
-      , "hallg": {"name": "Hall G"}
-      , "lounge": {"name": "Lounge"}
-      , "ambient": {"name": "Ambient"}
-      , "dome": {"name": "Dome"}
-      , "sendezentrum": {"name": "Sendezentrum"}
-      , "podcastertisch": {"name": "Sendezentrum - Podcastertisch"}
+      { "hall1":
+        { "name": "Hall 1: High Performance Firtzbox! Clusters"
+        , "translation": true
+        , "url": "https://cdn.c3voc.de/hls/s1_native_hd.m3u8"
+        }
+      , "hall2":
+        { "name": "Hall 2: Cyber-Außenpolitik"
+        , "translation": true
+        , "url": "https://cdn.c3voc.de/hls/s2_native_hd.m3u8"
+        }
+      , "hall6":
+        { "name": "Hall 6: From Nerdistan to Userland – A Travelreport"
+        , "translation": true
+        , "url": "https://cdn.c3voc.de/hls/s3_native_hd.m3u8"
+        }
+      , "hallg":
+        { "name": "Hall G: XXX"
+        , "translation": true
+        , "url": "https://cdn.c3voc.de/hls/s4_native_hd.m3u8"
+        }
+      , "lounge":
+        { "name": "Lounge"
+        , "translation": false
+        , "url": "https://cdn.c3voc.de/lounge.mp3"
+        }
+      , "ambient":
+        { "name": "Ambient"
+        , "translation": false
+        , "url": "https://cdn.c3voc.de/ambient.mp3"
+        }
+      , "dome":
+        { "name": "Dome"
+        , "translation": false
+        , "url": "https://cdn.c3voc.de/dome.mp3"
+        }
+      , "sendezentrum":
+        { "name": "Sendezentrum"
+        , "translation": false
+        , "url": "https://cdn.c3voc.de/hls/s5_native_hd.m3u8"
+        }
+      , "podcastertisch":
+        { "name": "Sendezentrum - Podcastertisch"
+        , "translation": false
+        , "url": "https://cdn.c3voc.de/hls/s6_native.mp3"
+        }
       }
     }));
   } else {
